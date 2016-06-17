731342faabe8f1439c9b295b2e78fa69cd084a6a
Peter Hayes <Peter.Hayes@pega.com>
2016-06-17 15:08:08 -0400

    Initial commit

---
+++ c/src/main/java/c/Main.java
@@ -0,0 +1,18 @@
+package c;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class Main {
+	public static void main(String[] args) {
+
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+
+        for(URL url: urls){
+        	String s = url.getFile();
+        	System.out.println(s.substring(s.lastIndexOf("/") + 1));
+        }
+	}
+}
