b48ac5626d6adc4adb5f45d69a87961b5bad5a23
Peter Hayes <Peter.Hayes@pega.com>
2016-06-17 17:31:16 -0400

    Changed log4j version and added fake junit test

--- c/src/main/java/c/Main.java
+++ c/src/main/java/c/Main.java
@@ -3,7 +3,11 @@ package c;
 import java.net.URL;
 import java.net.URLClassLoader;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+	private Logger logger;
+	
 	public static void main(String[] args) {
 
         ClassLoader cl = ClassLoader.getSystemClassLoader();
---
+++ c/src/test/java/c/FakeTest.java
@@ -0,0 +1,16 @@
+package c;
+
+import org.junit.Test;
+
+public class FakeTest {
+
+	@Test
+	public void dummy() {
+		System.out.println("This is a test");
+	}
+	
+	@Test
+	public void dummy2() {
+		System.out.println("This is a test too");
+	}
+}
